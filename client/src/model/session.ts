e574d7b8b0522f3e34264f5a8a571c9ea4498fcc
LengthyGamer <kollmar.peter0@gmail.com>
2023-10-16 14:09:12 -0400

    Nav Bar Update

--- client/src/model/session.ts
+++ client/src/model/session.ts
@@ -1,14 +1,29 @@
 /* B"H
 */
-import {reactive} from "vue";
+import { reactive } from "vue";
 
 const session = reactive({
-    user: null as User | null,
+  user: null as User | null,
 })
 
 export interface User {
     id?: string,
     firstName: string,
     lastName: string,
-    
+    email: string,
+    role: "admin" | "user",
+    token?: string
+}
+
+export function getSession(){
+  return session;
+}
+
+export function login(){
+  session.user = {
+    firstName: "Test",
+    lastName: "User",
+    email: "test@user.com",
+    role: "admin"
+  }
 }
