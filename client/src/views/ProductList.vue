8e110d62898e455b99ea732e39cee0cec526bf39
LengthyGamer <kollmar.peter0@gmail.com>
2023-10-22 20:02:09 -0400

    Update 3

--- client/src/views/ProductList.vue
+++ client/src/views/ProductList.vue
@@ -1,51 +1,65 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import { getProducts, type Product } from '@/model/products';
+import { ref } from 'vue'
 
-const products = ref([] as Product)
+const products = ref([] as Product[])
 const isLoading = ref(false)
 
-setTimeout(()=> products.value = getProducts(), 1000)
+isLoading.value = true;
+setTimeout(()=> { 
+    products.value = getProducts()
+    isLoading.value = false;
+  }, 1000)
+
 
 </script>
 
 <template>
-    <div>
-        <h1 class="title">
-            Product List
-        </h1>
-    </div>
+  <div>
+    <h1 class="title">
+      Product List
+    </h1>
     <progress v-if="isLoading" class="progress is-success" >Loading...</progress>
 
     <div class="product-list">
-        <div v-for="product in products" :key="product.id" class="product">
-            <img :src="product.thumbnail" :alt="product.title" />
-            
-        </div>
+      <div v-for="product in products" :key="product.id" class="product">
+        <img :src="product.thumbnail" :alt="product.title" />
+        <h3 class="title is-3">{{ product.title }}</h3>
+        <p>{{ product.description }}</p>
+        <p>
+          <span>$</span>
+          <i class="price">{{ product.price }}</i>
+          <button class="button is-success">+</button>
+        </p>
+      </div>
     </div>
+  </div>
 </template>
+
 
 <style scoped>
 
-.product-list{
-    display: flex;
-    flex-wrap: wrap;
-    background-color: aliceblue;
+.product-list {
+  display: flex;
+  flex-wrap: wrap;
+  background-color: aliceblue;
 }
 
 .product {
-    flex-basis: 15rem;
-    flex-grow: 1;
-    margin: 1rem;
-    padding: .5rem;
-    background-color: white;
-    border: 1px solid #ccc;
-    border-radius: .5rem;
-    box-shadow: 0 0 1rem #000;
+  flex-basis: 15rem;
+  flex-grow: 1;
+  margin: 1rem;
+  padding: .5rem;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: .5rem;
+  box-shadow: 0 0 1rem #0004;
 }
 
 .price {
-    font-size: 2em;
-    
+  font-size: 2em;
+  font-weight: bold;
+  margin-right: .5rem;
 }
 
 </style>
